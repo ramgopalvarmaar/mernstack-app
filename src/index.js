ec8ceea5bf98c85e604175c571e017b871450189
rarajendran <v-rarajendran@expedia.com>
2020-08-21 14:47:55 +0530

    Initial commit - cards are incorporated

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
+import App from "./components/App";
+
+import { Provider } from "react-redux";
+import store from "./store";
+
+import * as serviceWorker from "./serviceWorker";
+
+import "./index.css";
+
+const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  rootElement
 );
 
 // If you want your app to work offline and load faster, you can change
