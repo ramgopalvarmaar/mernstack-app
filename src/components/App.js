c18ce3f09b6f8c7a24d1d75c6c776f9cff08cbe4
rarajendran <v-rarajendran@expedia.com>
2020-09-11 19:04:35 +0530

    Code cleanup - Removed unused material-ui pages

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
 import "../styles/App.css";
 
 import React, { Component } from "react";
-import Base from "./Base";
 import SideBar from './SideBar';
 import Header from './HeaderComponent';
 import Tab from  './TabComponent';
