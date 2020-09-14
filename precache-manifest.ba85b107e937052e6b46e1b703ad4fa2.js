5316b87f3ca29d15f1a6f5462b5b011667ebe7ed
Sarath Vijayakumaran <sarathvk619@gmail.com>
2020-09-14 09:59:59 +0530

    Updates

---
+++ precache-manifest.ba85b107e937052e6b46e1b703ad4fa2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ae9325b9299a76562ba6a3f56288863",
+    "url": "/team-collaborator/index.html"
+  },
+  {
+    "revision": "bf4530f56146d276f4ac",
+    "url": "/team-collaborator/static/css/main.7724b860.chunk.css"
+  },
+  {
+    "revision": "e12c2130ab8b64503b0c",
+    "url": "/team-collaborator/static/js/2.c4b4094c.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/team-collaborator/static/js/2.c4b4094c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf4530f56146d276f4ac",
+    "url": "/team-collaborator/static/js/main.8d1ba64c.chunk.js"
+  },
+  {
+    "revision": "038f43839016c8429e8e",
+    "url": "/team-collaborator/static/js/runtime-main.ef9baa9d.js"
+  }
+]);
